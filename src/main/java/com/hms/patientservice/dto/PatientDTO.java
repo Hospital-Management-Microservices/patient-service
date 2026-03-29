3e9ce0f53d28d887eb251d03f5a7272fa4a3d536
methsu <thiyaana.vidanaarachchi@gmail.com>
2026-03-29 17:20:26 +0530

    Added DTO

---
+++ src/main/java/com/hms/patientservice/dto/PatientDTO.java
@@ -0,0 +1,28 @@
+package com.hms.patientservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PatientDTO {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    private String gender;
+    private String email;
+    private String phoneNumber;
+    private String address;
+    private String bloodGroup;
+    private String emergencyContact;
+    private Boolean isActive;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
